2a57577b98b2a85abeb088be73ef8ac4d6866fe8
Ginanjar Abdul Wahab <gin.bqs@gmail.com>
2016-02-09 17:06:43 +0700

    weer

---
+++ application/views/inventory/inv_barang/foto.php
@@ -0,0 +1,25 @@
+<img src="<?php echo base_url()?>public/files/foto/10/belanja.png " width="150px" height="100px"/>
+<?php
+  /* $folder = "10"; // Ubah nama foldernya
+   $handle = opendir($folder);
+   echo '<table cellspacing="2" cellpadding="5">';
+   echo '<tr>';
+   $i = 1;
+   $fileGambar = array('png', 'jpg', 'jpeg', 'gif');
+
+   while(false !== ($file = readdir($handle))){
+      $fileAndExt = explode('.', $file);
+      if(in_array(end($fileAndExt), $fileGambar)){
+         echo '<td style="border:1px solid #000000;" align="center">
+            <img src="<?php echo base_url()?>public/files/foto/10/'.$file.'" width="100" /><br />
+            '.$file.'
+         </td>';
+      if(($i % 4) == 0){
+         echo '</tr><tr>';
+      }
+      $i++;
+   }
+}
+   echo '</tr>';
+   echo '</table>';*/
+?>
