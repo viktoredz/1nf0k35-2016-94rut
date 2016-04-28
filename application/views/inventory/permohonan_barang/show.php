7a82b3454c6e0ae89dc06d37c01bf0931ec31076
ginanjar <gin.bqs@gmail.com>
2016-04-28 16:32:37 +0700

    update grid

--- application/views/inventory/permohonan_barang/show.php
+++ application/views/inventory/permohonan_barang/show.php
@@ -89,7 +89,7 @@
 				//var arr2 = $.map(arr[8], function(el) { return el });
 				//input data
 
-				$.post( '<?php echo base_url()?>inventory/permohonanbarang/updatestatus', {pilihan_status_pengadaan:arr[6],inv_permohonan_barang:arr[2]},function( data ) {
+				$.post( '<?php echo base_url()?>inventory/permohonanbarang/updatestatus', {pilihan_status_pengadaan:arr[7],inv_permohonan_barang:arr[2]},function( data ) {
 						$("#jqxgrid").jqxGrid('updateBoundData');
 						
 				 });
