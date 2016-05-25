b3994ca73b098378020f3c2fa7046014150dd0ec
Viktor <xu.redzonez@gmail.com>
2016-05-25 21:42:04 +0700

    DB Pangkat Jabatan

--- application/views/kepegawaian/drh/form_jabatan.php
+++ application/views/kepegawaian/drh/form_jabatan.php
@@ -1,6 +1,11 @@
 <script>
   $(function() {
         $('#jqxTabsJabatan').jqxTabs({ width: '100%', height: '500'});
+        $('#btn-skjabatan-tambah').click(function(){
+            $.get('<?php echo base_url()?>kepegawaian/drh_jabatann/add/{id}', function (data) {
+                $('#content5').html(data);
+            });
+        });
 
         var loadPage = function (url, tabIndex) {
             $.get(url, function (data) {
@@ -18,6 +23,11 @@
 </script>
 
 <section class="content">
+  <div class="row" style="margin-bottom:15px">
+    <div class="col-md-12" style="text-align: right">
+      <button type="button" class="btn btn-success" id="btn-skjabatan-tambah"><i class='fa fa-plus-circle'></i> &nbsp; Tambah SK Jabatan</button>
+    </div>
+  </div>
 <div id='jqxWidgetJabatan'>
     <div id='jqxTabsJabatan'>
         <ul>
@@ -36,24 +46,13 @@
                       <i class="icon fa fa-plus" style="font-size: 18px"></i>
                   </div>
                   <div style="margin-left: 10px; vertical-align: middle; text-align: center; float: left;">
-                      Jabatan Fungsional Umum</div>
-              </div>
-            </li>
-            <li style="margin-left: 15px;">
-              <div style="height: 20px; margin-top: 5px;">
-                  <div style="float: left;">
-                      <i class="icon fa fa-plus" style="font-size: 18px"></i>
-                  </div>
-                  <div style="margin-left: 10px; vertical-align: middle; text-align: center; float: left;">
-                      Jabatan Fungsional Tertentu</div>
+                      Jabatan Fungsional</div>
               </div>
             </li>
         </ul>
         <div id="jabatannsub1" style="background: #FAFAFA">
         </div>
         <div id="jabatannsub2" style="background: #FAFAFA">
-        </div>
-        <div id="jabatannsub3" style="background: #FAFAFA">
         </div>
     </div>
 </div>
